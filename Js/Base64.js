a8d52a72177370ef59485d585b110dccdca4133d
Naufra1 <gustavoresende3e@gmail.com>
2022-10-17 20:20:39 -0300

    Concertado o Bug

--- Js/Base64.js
+++ Js/Base64.js
@@ -1,48 +1,29 @@
 let cod = document.getElementById("cifras")
 let btnCod = document.getElementById("traduzir")
-const $tipoCod=document.querySelector(codififar);
 
-function codBtn(){
-    btnCod.innerHTML = "Codificar Mensagem!"
-    
-    btnCod.addEventListener("click", () => {
-if($tipoCod.value==="codificar"){
-//pregunta codigo cesar ou base 64
-
-
-
-}else{
-//pregunta codigo cesar ou base 64
-
-
-}
-
-        let cryptValue = cod.value
-
-        if (cryptValue === "Base64"){
-            let str = document.getElementById("codigo")
-            let cod1 = str.value
-            let res = document.getElementById("resultado")
-            res.innerHTML = btoa(cod1)
-        }else {
-            console.log("asdad")
-        }
-    } )
-}
-
-function decodBtn(){
-    btnCod.innerHTML = "Decodificar Mensagem!"
-
-    btnCod.addEventListener("click", () => {
-        let cryptValue = cod.value
-        
-        if (cryptValue === "Base64"){
-            let str = document.getElementById("codigo")
-            let cod1 = str.value
-            let res = document.getElementById("resultado")
+//Botão para Traduzir  
+btnCod.addEventListener("click", (event) => {
+    event.preventDefault()
+    let str = document.getElementById("codigo")
+    let cod1 = str.value
+    let res = document.getElementById("resultado")
+    let codInp = document.querySelector('input[name = "tipocod"]:checked').value
+    //Codificação da Base64
+    if (cod.value === "Base64"){
+        if(codInp === "c"){
+        res.innerHTML = btoa(cod1)
+    }else if(codInp === "d"){
             res.innerHTML = atob(cod1)
-        }else {
-             console.log("fasdad")
         }
-    } )
+    } else{
+        
+    }
+} )
+
+function codTxt(){
+    btnCod.innerHTML = "Codificar Mensagem!"
+}
+
+function decodTxt(){
+    btnCod.innerHTML = "Decodificar Mensagem!"
 }
